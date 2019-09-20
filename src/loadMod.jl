a90dd5ef04211b905c5ca5a4a3a5a0b34b17a7fd
Haoxiang Yang <haoxiangyang89@gmail.com>
2019-09-20 15:41:27 -0400

    Add loading the extensive formulation

--- src/loadMod.jl
+++ src/loadMod.jl
@@ -5,6 +5,7 @@ using JuMP, Ipopt, Gurobi, Combinatorics, JLD, HDF5, DelimitedFiles;
 include("def.jl");
 include("readin.jl");
 include("main.jl");
+include("extForm.jl");
 include("auxiliary.jl");
 include("forwardPass.jl");
 include("backwardPass.jl");
