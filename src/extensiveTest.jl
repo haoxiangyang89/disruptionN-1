2f83d6bcc705f91d649d5e826c72eb6a559898f8
Haoxiang Yang <haoxiangyang89@gmail.com>
2019-10-01 09:18:35 -0600

    Change the solver for all scripts

--- src/extensiveTest.jl
+++ src/extensiveTest.jl
@@ -1,11 +1,13 @@
 # test 1: compare SDDP and extensive formulation
-include("run_small.jl");
+include("run.jl");
 
 mp = Model();
 startTE = time();
 mp = extForm(mp, 1, 0, [[],bData.bInv,[]], 1, τ, Δt, T, fData, bData, dData, pDistr);
-optimize!(mp, with_optimizer(Gurobi.Optimizer, GUROBI_ENV, noThreads = 3, OutputFlag = 0));
-    QCPDual = 1, NumericFocus = 3, BarQCPConvTol = 1e-9, FeasibilityTol = 1e-9));
+# optimize!(mp, with_optimizer(Gurobi.Optimizer, GUROBI_ENV, OutputFlag = 0,
+#     QCPDual = 1, NumericFocus = 3, BarQCPConvTol = 1e-9, FeasibilityTol = 1e-9));
+optimize!(mp, with_optimizer(Ipopt.Optimizer, linear_solver = "ma27", acceptable_tol = 1e-8));
+
 elapsedTE = time() - startTE;
 mpObj = objective_value(mp);
 
--- src/forwardPass.jl
+++ src/forwardPass.jl
@@ -93,7 +93,7 @@ function noDisruptionBuild(Δt, T, fData, bData, dData, pDistr, cutDict, solveOp
         # solve the problem
         # optimize!(mp, with_optimizer(Gurobi.Optimizer, GUROBI_ENV, OutputFlag = 0,
         #     QCPDual = 1, NumericFocus = 3, BarQCPConvTol = 1e-9, FeasibilityTol = 1e-9));
-        optimize!(mp, with_optimizer(Ipopt.Optimizer, linear_solver = "ma27"));
+        optimize!(mp, with_optimizer(Ipopt.Optimizer, linear_solver = "ma27", acceptable_tol = 1e-8, print_level = 0));
         mpObj = objective_value(mp);
         # obtain the solutions
         solSp = Dict();
