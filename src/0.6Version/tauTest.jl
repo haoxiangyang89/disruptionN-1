9d5a8772623916bda0bef3932fcc611d424f115a
Haoxiang Yang <haoxiangyang89@gmail.com>
2019-10-17 19:22:54 -0600

    Add tauTest

--- src/0.6Version/tauTest.jl
+++ src/0.6Version/tauTest.jl
@@ -19,22 +19,19 @@ N = 30;
 pDistr = modifyT(pDistr,4/T,T);
 
 τList = [2,4,6,8,10];
-data = [];
+data = Dict();
 for τ in τList
-    cutDict,LBHist,UBHist,UBuHist,UBlHist = solveMain(τ, T, Δt, fData, pDistr, bData, dData, N, 20, 20);
-    push!(data,(cutDict,LBHist,UBHist,UBuHist,UBlHist));
+    cutDict,LBHist,UBHist,UBuHist,UBlHist = solveMain(τ, T, Δt, fData, pDistr, bData, dData, N, false, 20, 20);
+    pathListData = pmap(i -> simuPath(τ,T,pDistr), 1:NN);
+    pathDict = Dict();
+    for i in 1:NN
+        pathDict[i] = pathListData[i];
+    end
+    solSDDP, LBSDDP, costSDDP = exeForward(τ, T, Δt, fData, bData, dData, pDistr, NN, cutDict, pathDict);
+    listSDDP = [costSDDP[i] for i in 1:NN];
+    meanSDDP = mean(listSDDP);
+    sigmaSDDP = std(listSDDP);
+    data[τ] = [LBHist,LBSDDP,listSDDP,meanSDDP,sigmaSDDP];
 end
 
-cutDictT = Dict();
-LBHistT = Dict();
-UBHistT = Dict();
-UBuHistT = Dict();
-UBlHistT = Dict();
-for τ in 0:Int64(round(T/2))
-    cutDictT[τ],LBHistT[τ],UBHistT[τ],UBuHistT[τ],UBlHistT[τ] =
-        solveMain(τ, T, Δt, fData, pDistr, bData, dData, N);
-    println("Disruption Length $(τ): LB = $(LBHistT[τ][length(LBHistT[τ])])");
-end
-
-outputData = [cutDictT,LBHistT,UBHistT,UBuHistT,UBlHistT];
-save("tauOut.jld","data",outputData);
+save("tauResults.jld","data",data);
