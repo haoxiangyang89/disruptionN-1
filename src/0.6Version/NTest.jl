a2a81b0fc42e83d5f415fefe5dedb4c279b7fe94
Haoxiang Yang <haoxiangyang89@gmail.com>
2019-11-25 11:42:44 -0700

    Update NTest

--- src/0.6Version/NTest.jl
+++ src/0.6Version/NTest.jl
@@ -3,6 +3,7 @@
 addprocs(30);
 @everywhere include("loadMod.jl");
 @everywhere const GUROBI_ENV = Gurobi.Env();
+pmap(i -> importIpopt(),1:30);
 
 NList = [1,5,10,20,30];
 dataList = Dict();
@@ -17,11 +18,11 @@ for ci in 1:length(caseList)
         remotecall_fetch(readInData,j,ci,caseList,T);
     end
 
-    cutDict,LBHist,UBHist,UBuHist,UBlHist,timeHist = solveMain(τ, T, Δt, 3, false, 2, 2);
+    cutDict,LBHist,UBHist,UBuHist,UBlHist,timeHist = solveMain(τ, T, Δt, 30, false,false, 2, 2);
 
     for N in NList
         startT = time();
-        cutDict,LBHist,UBHist,UBuHist,UBlHist,timeHist = solveMain(τ, T, Δt, N, false, max(Int64(round(500/N)),20), max(Int64(round(500/N)),20));
+        cutDict,LBHist,UBHist,UBuHist,UBlHist,timeHist = solveMain(τ, T, Δt, N, false, false, max(Int64(round(500/N)),20), max(Int64(round(500/N)),20));
         elapsedT = time() - startT;
         dataList[N] = [LBHist,UBHist,UBuHist,UBlHist,timeHist,elapsedT];
 
