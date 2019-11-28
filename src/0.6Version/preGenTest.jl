060318d6751b2730971243ddf24f0fa0bf71e3ba
Haoxiang Yang <haoxiangyang89@gmail.com>
2019-11-28 00:46:22 -0700

    Add preGenTest

--- src/0.6Version/preGenTest.jl
+++ src/0.6Version/preGenTest.jl
@@ -1,4 +1,4 @@
-# test to compare allGen and only disruption gen
+# test to check if preGen helps reduce the solution time
 addprocs(30);
 @everywhere include("loadMod.jl");
 @everywhere const GUROBI_ENV = Gurobi.Env();
@@ -10,6 +10,7 @@ caseList = [13,33,123];
 T = 24;
 τ = Int64(1/6*T);
 Δt = 0.25;
+iterMax = 20;
 
 for ci in 1:length(caseList)
     for j in procs()
@@ -23,12 +24,16 @@ for ci in 1:length(caseList)
     elapsedT = time() - startT;
     dataList["dOnly"] = [LBHist,UBHist,UBuHist,UBlHist,timeHist,elapsedT];
 
-    save("dOnlyResults_$(ci).jld","data",dataList);
+    save("pgResults_$(ci).jld","data",dataList);
+
+    startPGT = time();
+    cutDictPG = preGen(τ, T, Δt, N, iterMax);
+    preGenT = time() - startPGT;
 
     startT = time();
-    cutDict,LBHist,UBHist,UBuHist,UBlHist,timeHist = solveMain(τ, T, Δt, N, true, false, max(Int64(round(200/N)),20), max(Int64(round(200/N)),20));
+    cutDict,LBHist,UBHist,UBuHist,UBlHist,timeHist = solveMain(τ, T, Δt, N, false, false, max(Int64(round(200/N)),20), max(Int64(round(200/N)),20),cutDictPG);
     elapsedT = time() - startT;
-    dataList["allGen"] = [LBHist,UBHist,UBuHist,UBlHist,timeHist,elapsedT];
+    dataList["preGen"] = [LBHist,UBHist,UBuHist,UBlHist,timeHist,elapsedT,preGenT];
 
-    save("dOnlyResults_$(ci).jld","data",dataList);
+    save("pgResults_$(ci).jld","data",dataList);
 end
