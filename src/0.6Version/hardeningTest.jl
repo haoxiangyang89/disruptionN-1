335fdf9f521d57c97855d7bbe86bdf5e74a0d9d9
Haoxiang Yang <haoxiangyang89@gmail.com>
2019-11-29 13:26:16 -0700

    Add hardening test

--- src/0.6Version/hardeningTest.jl
+++ src/0.6Version/hardeningTest.jl
@@ -3,37 +3,27 @@ addprocs(30);
 @everywhere include("loadMod.jl");
 @everywhere const GUROBI_ENV = Gurobi.Env();
 
-fileAdd = "case13_ieee.m";
-fData = readStatic(fileAdd,10000);
-disAdd = "testProbRead_96.csv"
-pDistr = readDisruption(disAdd,"csv");
-pAdd = "testDataP_96_Fixed.csv";
-qAdd = "testDataQ_96_Fixed.csv";
-dData = readDemand(pAdd,qAdd,"csv");
-bAdd = "testDataB.csv";
-bData = readBattery(bAdd,"csv");
+N = 5;
+dataList = Dict();
 
+caseList = [13,33,123];
 Δt = 0.25;
-N = 30;
-T = 12;
-τ = Int64(1/6*T);
-pDistr = modifyT(pDistr,4/T,T);
+iterMax = 20;
+T = 24;
 
-# obtain the no hardening costs for comparison
-ΩSet = [3,5,(2,9),(8,12),(10,13)];
-for ω in keys(pDistr.ωDistrn)
-    if !(ω in ΩSet)
-        pDistr = modifyOmega(pDistr,ω);
+for ci in 1:length(caseList)
+    τ = Int64(1/6*T);
+    for j in procs()
+        remotecall_fetch(readInData,j,ci,caseList,T);
     end
-end
-cutDict,LBHist,UBHist,UBuHist,UBlHist = solveMain(τ, T, Δt, fData, pDistr, bData, dData, N, false, 20, 20);
-data = Dict();
-data[0] = [cutDict,LBHist,UBHist,UBuHist,UBlHist];
 
-for ω in ΩSet
-    pDistrNew = modifyOmega(pDistr,ω);
-    cutDict,LBHist,UBHist,UBuHist,UBlHist = solveMain(τ, T, Δt, fData, pDistrNew, bData, dData, N, false, 20, 20);
-    # cutDict,LBHist,UBHist,UBuHist,UBlHist = solveMain(τ, T, Δt, fData, pDistr, bData, dData, N, false, 20, 20, Dict(), 0, [ω]);
-    data[ω] = [cutDict,LBHist,UBHist,UBuHist,UBlHist];
-    save("hardOut_10000.jld","data",data);
+    for ω in keys(pDistr.ωDistrn)
+        cutDictPG = preGen(τ, T, Δt, N, iterMax, false, Dict(), [ω]);
+
+        cutDict,LBHist,UBHist,UBuHist,UBlHist,timeHist = solveMain(τ, T, Δt, N, false, false,
+            max(Int64(round(200/N)),20), max(Int64(round(200/N)),20), cutDictPG, false, 0, [ω]);
+        dataList[ω] = [LBHist,UBHist,UBuHist,UBlHist,timeHist];
+    end
+
+    save("hardResults.jld","data",dataList);
 end
