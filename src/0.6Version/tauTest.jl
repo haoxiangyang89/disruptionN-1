5cd2294ecfbbef7f2d2026927fdec818f88b9ba8
Haoxiang Yang <haoxiangyang89@gmail.com>
2019-11-29 15:09:23 -0700

    Fixed tau test

--- src/0.6Version/tauTest.jl
+++ src/0.6Version/tauTest.jl
@@ -1,37 +1,40 @@
-# test 4a: effectiveness of different τ
+# test effectiveness of different τ
 addprocs(30);
 @everywhere include("loadMod.jl");
 @everywhere const GUROBI_ENV = Gurobi.Env();
 
-fileAdd = "case13_ieee.m";
-fData = readStatic(fileAdd,500);
-disAdd = "testProbRead_96.csv"
-pDistr = readDisruption(disAdd,"csv");
-pAdd = "testDataP_96_Fixed.csv";
-qAdd = "testDataQ_96_Fixed.csv";
-dData = readDemand(pAdd,qAdd,"csv");
-bAdd = "testDataB.csv";
-bData = readBattery(bAdd,"csv");
+N = 5;
+dataList = Dict();
 
-T = 24;
+caseList = [13,33,123];
 Δt = 0.25;
-N = 30;
+iterMax = 20;
+T = 24;
 NN = 1000;
-pDistr = modifyT(pDistr,4/T,T);
 
 τList = [2,4,6,8,10];
-data = Dict();
-for τ in τList
-    cutDict,LBHist,UBHist,UBuHist,UBlHist = solveMain(τ, T, Δt, fData, pDistr, bData, dData, N, false, 20, 20);
-    pathListData = pmap(i -> simuPath(τ,T,pDistr), 1:NN);
-    pathDict = Dict();
-    for i in 1:NN
-        pathDict[i] = pathListData[i];
+
+for ci in 1:length(caseList)
+    dataList[ci] = Dict();
+    for j in procs()
+        remotecall_fetch(readInData,j,ci,caseList,T);
     end
-    solSDDP, LBSDDP, costSDDP = exeForward(τ, T, Δt, fData, bData, dData, pDistr, NN, cutDict, pathDict);
-    listSDDP = [costSDDP[i] for i in 1:NN];
-    meanSDDP = mean(listSDDP);
-    sigmaSDDP = std(listSDDP);
-    data[τ] = [LBHist,LBSDDP,listSDDP,meanSDDP,sigmaSDDP];
-    save("tauResults.jld","data",data);
+
+    for τ in τList
+        # train the stochastic strategy
+        cutDictPG = preGen(τ, T, Δt, N, iterMax);
+        cutDict,LBHist,UBHist,UBuHist,UBlHist,timeHist = solveMain(τ, T, Δt, N, false, false, max(Int64(round(200/N)),20), max(Int64(round(200/N)),20),cutDictPG);
+
+        pathListData = pmap(i -> simuPath(τ,T,pDistr), 1:NN);
+        pathDict = Dict();
+        for i in 1:NN
+            pathDict[i] = pathListData[i];
+        end
+        solSDDP, LBSDDP, costSDDP = exeForward(τ, T, Δt, NN, false, pathDict);
+        listSDDP = [costSDDP[i] for i in 1:NN];
+        meanSDDP = mean(listSDDP);
+        sigmaSDDP = std(listSDDP);
+        dataList[ci][τ] = [LBHist,LBSDDP,listSDDP,meanSDDP,sigmaSDDP];
+        save("tauResults.jld","data",dataList);
+    end
 end
