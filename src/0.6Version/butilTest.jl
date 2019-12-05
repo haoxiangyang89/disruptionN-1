8e8eef8a088d6cda5a1dec5f5e5ef070c3b74b54
Haoxiang Yang <haoxiangyang89@gmail.com>
2019-12-04 17:58:21 -0700

    Added all csv files for plotting

--- src/0.6Version/butilTest.jl
+++ src/0.6Version/butilTest.jl
@@ -64,8 +64,8 @@ for ci in 1:length(caseList)
                 # obtain the inventory & actual injection to network until the next solution
                 for t in dDetList[j]:(dDetList[j+1] - 1)
                     for b in bData.IDList
-                        zDet[i][b][t] = solDet[i][j][1].zp;
-                        wDet[i][b][t] = solDet[i][j][1].w;
+                        zDet[i][b][t] = solDet[i][j][1].zp[b,t];
+                        wDet[i][b][t] = solDet[i][j][1].w[b,t];
                     end
                 end
             end
@@ -76,10 +76,43 @@ for ci in 1:length(caseList)
                 # obtain the inventory & actual injection to network until the next solution
                 for t in dSDDPList[j]:(dSDDPList[j+1] - 1)
                     for b in bData.IDList
-                        zSDDP[i][b][t] = solSDDP[i][j][1].zp;
-                        wSDDP[i][b][t] = solSDDP[i][j][1].w;
+                        zSDDP[i][b][t] = solSDDP[i][j][1].zp[b,t];
+                        wSDDP[i][b][t] = solSDDP[i][j][1].w[b,t];
                     end
                 end
+            end
+            # obtain the statistics to show the utilization
+            for b in bData.IDList
+                wListD = [wDet[i][b][t] for t in 1:T];
+                meanwD = mean(wListD);
+                wListS = [wSDDP[i][b][t] for t in 1:T];
+                meanwS = mean(wListS);
+                chargeD = 0;
+                chargeS = 0;
+                dischargeD = 0;
+                dischargeS = 0;
+                cdCoeff = [];
+                wDet[i][b][0] = bData.bInv[b];
+                wSDDP[i][b][0] = bData.bInv[b];
+                for t in 1:T
+                    yD = wDet[i][b][t] - wDet[i][b][t - 1];
+                    zD = zDet[i][b][t];
+                    if zD > 0
+                        dischargeD += zD;
+                    else
+                        chargeD += zD;
+                    end
+
+                    yS = (wSDDP[i][b][t] - wSDDP[i][b][t - 1])/Δt;
+                    zS = zSDDP[i][b][t];
+                    push!(cdCoeff,zS/yS);
+                    if zS > 0
+                        dischargeS += zS;
+                    else
+                        chargeS += zS;
+                    end
+                end
+                println(b," ",[meanwD,chargeD,dischargeD],[meanwS,chargeS,dischargeS,cdCoeff]);
             end
         end
         save("butilResults.jld","detOut",[uDet,zDet,wDet],"stochOut",[uSDDP,zSDDP,wSDDP]);
