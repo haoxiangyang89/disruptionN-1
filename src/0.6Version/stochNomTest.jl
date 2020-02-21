dc4f4e0ae1dd642d59fa0183a92009d0a9dc0b30
Haoxiang Yang <haoxiangyang89@gmail.com>
2020-02-21 15:02:11 -0700

    Finish the stochNomTest

--- src/0.6Version/stochNomTest.jl
+++ src/0.6Version/stochNomTest.jl
@@ -20,17 +20,30 @@ for ci in 1:length(caseList)
             remotecall_fetch(readInData,j,ci,caseList,T);
             #remotecall_fetch(readInData_old,j,T,ωSet0,10000,0);
         end
+        pathDict = pathDictA[T];
+        pathDict1 = Dict();
+        noP = 1;
+        foundBool = false;
+        while !(foundBool)
+            if length(pathDict[noP]) == 1
+                pathDict1[1] = pathDict[noP];
+                foundBool = true;
+            else
+                noP += 1;
+            end
+        end
 
         # train the stochastic programming strategy
         cutDictPG = preGen(τ, T, Δt, N, iterMax);
         # cutDict,LBHist,UBHist,UBuHist,UBlHist,timeHist = solveMain(τ, T, Δt, N, true, false, 10, 10, cutDictPG);
         cutDict,LBHist,UBHist,UBuHist,UBlHist,timeHist = solveMain(τ, T, Δt, N, false, false,
-            max(Int64(round(500/N)),20), max(Int64(round(500/N)),20),cutDictPG);
+            max(Int64(round(200/N)),20), max(Int64(round(200/N)),20),cutDictPG);
         for j in procs()
             remotecall_fetch(cutIni,j,cutDict);
         end
 
-
+        solSDDP, LBSDDP, costSDDP = exeForward(τ, T, Δt, 1, false, pathDict1);
+        stochNomOut[T] = [solSDDP,LBSDDP,costSDDP];
 
         save("stochNomResults_$(ci).jld", "stochNomOut", stochNomOut);
     end
