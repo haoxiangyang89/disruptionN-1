6c6569b6eeb709a6ef96c130ca98cc0225fc4580
Haoxiang Yang <haoxiangyang89@gmail.com>
2020-07-28 00:10:29 -0600

    Fixed the parametrization + a few bugs

--- oscar/sddp.jl
+++ oscar/sddp.jl
@@ -47,7 +47,7 @@ function main()
         optimizer = () -> Gurobi.Optimizer(GRB),
     ) do subproblem, node
         t, disruption = node.stage, node.disruption_length
-        set_silent(sp)
+        #set_silent(sp)
 
         # State variables
         @variable(
@@ -57,7 +57,7 @@ function main()
             initial_value = 0.0
         )
         @constraint(subproblem, spUb[g in gens], Sp[g].out <= gen_data[g]["pmax"]);
-        @constraint(subproblem, spLb[g in gens], Sp[g].out >= gen_data[g]["pmax"]);
+        @constraint(subproblem, spLb[g in gens], Sp[g].out >= gen_data[g]["pmin"]);
         @variable(
             subproblem,
             Sq[g in gens],
@@ -65,7 +65,7 @@ function main()
             initial_value = 0.0
         )
         @constraint(subproblem, sqUb[g in gens], Sq[g].out <= gen_data[g]["qmax"]);
-        @constraint(subproblem, sqLb[g in gens], Sq[g].out >= gen_data[g]["qmax"]);
+        @constraint(subproblem, sqLb[g in gens], Sq[g].out >= gen_data[g]["qmin"]);
         @variable(
             subproblem,
             0 <= w[i in batteries] <= battery_data.capacity[i],
@@ -111,8 +111,8 @@ function main()
                 sum(Sp[g].out for g in gens if gen_data[g]["gen_bus"] == i) -
                 data.P[parse(Int, i), t] +
                 L[i, :p, :+] - L[i, :p, :-] ==
-                sum(P[l, t] for l in lines if from(l) == i) -
-                sum(P[l, t] for l in lines if to(l) == i)
+                sum(P[l] for l in lines if from(l) == i) -
+                sum(P[l] for l in lines if to(l) == i)
         )
         @constraint(
             subproblem,
@@ -121,8 +121,8 @@ function main()
                 sum(Sq[g].out for g in gens if gen_data[g]["gen_bus"] == i) -
                 data.Q[parse(Int, i), t] +
                 L[i, :q, :+] - L[i, :q, :-] ==
-                sum(Q[l, t] for l in lines if from(l) == i) -
-                sum(Q[l, t] for l in lines if to(l) == i)
+                sum(Q[l] for l in lines if from(l) == i) -
+                sum(Q[l] for l in lines if to(l) == i)
         )
 
         # LinDist Flow power flow equations
@@ -134,9 +134,9 @@ function main()
         @constraints(subproblem, begin
             # Thermal limits
             [l = lines], [W[l], P[l], Q[l]] in SecondOrderCone()
-            [b = batteries], [u.out[b], zp[b], zq[b]] in SecondOrderCone()
+            [b = batteries], [u[b].out, zp[b], zq[b]] in SecondOrderCone()
             # battery efficiency curve
-            [b = batteries, l in 1:length(battery_data.ηα[b])], zp[b] <= battery_data.ηα[b][l]*y[b] + battery_data.ηβ[b][l])
+            [b = batteries, l in 1:length(battery_data.ηα[b])], zp[b] <= battery_data.ηα[b][l]*y[b] + battery_data.ηβ[b][l]
         end)
         # thermal limits under the normal condition
         @constraint(subproblem, thermal[l = lines], W[l] == line_data[l]["rate_a"]);
@@ -151,17 +151,27 @@ function main()
         # Generator ramp down limit
 
 
-        @stageobjective(sp, 0.0)
-        SDDP.parameterize(sp, data.support, data.nominal_probability) do ω
+        @stageobjective(subproblem, 0.0)
+        SDDP.parameterize(subproblem, data.support, data.nominal_probability) do ω
             if ω isa Int
                 # Change generator bounds
-                JuMP.set_normalized_rhs(spUb[ω], 0)
-                JuMP.set_normalized_rhs(sqUb[ω], 0)
-                JuMP.set_normalized_rhs(spLb[ω], 0)
-                JuMP.set_normalized_rhs(sqLb[ω], 0)
-
-                JuMP.set_normalized_rhs(genRamp_up[ω], 1000*gen_data[g]["ramp_agc"])
-                JuMP.set_normalized_rhs(genRamp_down[ω], -1000*gen_data[g]["ramp_agc"])
+                for g in gens
+                    if g == ω
+                        JuMP.set_normalized_rhs(spUb[ω], 0)
+                        JuMP.set_normalized_rhs(sqUb[ω], 0)
+                        JuMP.set_normalized_rhs(spLb[ω], 0)
+                        JuMP.set_normalized_rhs(sqLb[ω], 0)
+                        JuMP.set_normalized_rhs(genRamp_up[ω], 1000*gen_data[g]["ramp_agc"])
+                        JuMP.set_normalized_rhs(genRamp_down[ω], -1000*gen_data[g]["ramp_agc"])
+                    else
+                        JuMP.set_normalized_rhs(spUb[ω], gen_data[g]["pmax"])
+                        JuMP.set_normalized_rhs(sqUb[ω], gen_data[g]["qmax"])
+                        JuMP.set_normalized_rhs(spLb[ω], gen_data[g]["pmin"])
+                        JuMP.set_normalized_rhs(sqLb[ω], gen_data[g]["qmin"])
+                        JuMP.set_normalized_rhs(genRamp_up[ω], gen_data[g]["ramp_agc"])
+                        JuMP.set_normalized_rhs(genRamp_down[ω], -gen_data[g]["ramp_agc"])
+                    end
+                end
             else
                 # Change line bounds
                 if (ω.first,ω.second) in keys(rev_line)
@@ -169,9 +179,17 @@ function main()
                 else
                     line_down = rev_line((ω.second,ω.first));
                 end
-                JuMP.set_normalized_rhs(linDistFlow_ub[line_down], -1000*line_data[l]["rate_a"]);
-                JuMP.set_normalized_rhs(linDistFlow_lb[line_down], 1000*line_data[l]["rate_a"]);
-                JuMP.set_normalized_rhs(thermal[l], 0.0);
+                for l in lines
+                    if l == line_down
+                        JuMP.set_normalized_rhs(linDistFlow_ub[line_down], -1000*line_data[l]["rate_a"]);
+                        JuMP.set_normalized_rhs(linDistFlow_lb[line_down], 1000*line_data[l]["rate_a"]);
+                        JuMP.set_normalized_rhs(thermal[l], 0.0);
+                    else
+                        JuMP.set_normalized_rhs(linDistFlow_ub[line_down], 0);
+                        JuMP.set_normalized_rhs(linDistFlow_lb[line_down], 0);
+                        JuMP.set_normalized_rhs(thermal[l], line_data[l]["rate_a"]);
+                    end
+                end
             end
         end
     end
