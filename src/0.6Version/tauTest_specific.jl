5042ab631f925cd6ae4129d21429a747282f7a1e
Haoxiang Yang <haoxiangyang89@gmail.com>
2020-09-21 14:07:12 -0600

    Fixed the tauTest_specific

--- src/0.6Version/tauTest_specific.jl
+++ src/0.6Version/tauTest_specific.jl
@@ -32,34 +32,20 @@ for ci in 1:length(caseList)
     meanSDDP = mean(listSDDP);
     sigmaSDDP = std(listSDDP);
 
-    dataList[ci] = [LBHist,LBSDDP,listSDDP,meanSDDP,sigmaSDDP];
-
-    save("tauResults_specific.jld","data",dataList);
-end
-
-dataListSame = Dict();
-for ci in 1:length(caseList)
     for j in procs()
         remotecall_fetch(readInData_tau,j,ci,caseList,T,1e4,0,"Same");
-        #remotecall_fetch(readInData_old,j,T,ωSet0,10000,0);
     end
 
-    # train the stochastic strategy
-    # cutDict,LBHist,UBHist,UBuHist,UBlHist,timeHist = solveMain(τ, T, Δt, N, true, false, 20, 20, cutDictPG);
-    cutDict,LBHist,UBHist,UBuHist,UBlHist,timeHist = solveMain(T, Δt, N, false, false,
+    cutDicts,LBHists,UBHists,UBuHists,UBlHists,timeHists = solveMain(T, Δt, N, false, false,
         max(Int64(round(500/N)),20), max(Int64(round(500/N)),20),Dict());
 
-    pathListData = pmap(i -> simuPath(T,pDistr), 1:NN);
-    pathDict = Dict();
-    for i in 1:NN
-        pathDict[i] = pathListData[i];
-    end
-    solSDDP, LBSDDP, costSDDP = exeForward(T, Δt, NN, false, pathDict);
-    listSDDP = [costSDDP[i] for i in 1:NN];
-    meanSDDP = mean(listSDDP);
-    sigmaSDDP = std(listSDDP);
+    solSDDPs, LBSDDPs, costSDDPs = exeForward(T, Δt, NN, false, pathDict);
+    listSDDPs = [costSDDPs[i] for i in 1:NN];
+    meanSDDPs = mean(listSDDPs);
+    sigmaSDDPs = std(listSDDPs);
 
-    dataListSame[ci] = [LBHist,LBSDDP,listSDDP,meanSDDP,sigmaSDDP];
+    dataList[ci] = [LBHist,LBSDDP,listSDDP,meanSDDP,sigmaSDDP,
+                    LBHists,LBSDDPs,listSDDPs,meanSDDPs,sigmaSDDPs];
 
-    save("tauResults_specificSame.jld","data",dataListSame);
+    save("tauResults_specific.jld","data",dataList);
 end
